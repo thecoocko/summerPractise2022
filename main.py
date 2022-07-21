756ee1e55f1b494087c3aacbf8c7c486751b7dd3
coocko <butenkonaddezhda@gmail.com>
2022-07-21 17:29:04 +0200

    folders

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plot
 import pandas as pd
 import numpy as np
-import regression as myRegression
+import modules.regression as mreg
+import modules.neuron_network as nn
+import modules.evolution_algorithm as ea
+
+mreg.hi('asd')
+nn.hi('asdsadsd')
+ea.hi('asddddd')
