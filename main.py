40879c6ef8398f23cf811fc5f8efd0d12c743fc5
coocko <butenkonaddezhda@gmail.com>
2022-07-22 13:08:31 +0200

    clear data

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import modules.regression as mreg
 import modules.neuron_network as nn
 import modules.evolution_algorithm as ea
 
-mreg.hi('asd')
-nn.hi('asdsadsd')
-ea.hi('asddddd')
+df = pd.read_csv('../Foreign_Exchange_Rates.csv', parse_dates=True, index_col=0)
+print(f"DATASET SHAPE: {df.shape}")
+
+
+
+
+for i in range(1,len(df.columns)):
+    df[df.columns[i]] = df[df.columns[i]].replace('ND',0).astype('float')
+    
+print(df.dtypes)
+
+mreg.hi('mreg')
+nn.hi('nn')
+ea.hi('ea')
