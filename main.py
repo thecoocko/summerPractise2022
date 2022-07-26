9ded1f5dc74e9239d07ec011880b84f4ec090e2b
coocko <butenkonaddezhda@gmail.com>
2022-07-26 20:46:41 +0200

    govno kakoeto

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import modules.regression as mreg
-import modules.neuron_network as nn
-import modules.evolution_algorithm as ea
+from scipy import stats
+import itertools
+import statsmodels.api as sm
+import statsmodels.tsa as ts
+import warnings
 
-df = pd.read_csv('../Foreign_Exchange_Rates.csv', parse_dates=True, index_col=0)
+df = pd.read_csv('../Foreign_Exchange_Rates.csv')
 print(f"DATASET SHAPE: {df.shape}")
+print(df)
 
+CURRENCY_DATA = df[['Time Serie', 'TAIWAN - NEW TAIWAN DOLLAR/US$']]
+print(CURRENCY_DATA.shape)
+for i in range(1,len(CURRENCY_DATA.columns)):
+    CURRENCY_DATA[CURRENCY_DATA.columns[i]] = CURRENCY_DATA[CURRENCY_DATA.columns[i]].replace('ND',0).astype('float')
 
+def defaultPlot():
+    plt.figure(figsize=(16,8))
+    plt.plot(CURRENCY_DATA)
+    plt.show()
 
+def autoAndPartcorrelation():
+    _ = plt.figure(figsize=(18,10))
+    ax_1 = plt.subplot(2,1,1)
+    _ = sm.graphics.tsa.plot_acf(CURRENCY_DATA.values.squeeze(), lags=50, ax=ax_1)
+    ax_2 = plt.subplot(2,1,2)
+    _ = sm.graphics.tsa.plot_pacf(CURRENCY_DATA.values.squeeze(), lags=50, ax=ax_2)
+    plt.show()
 
-for i in range(1,len(df.columns)):
-    df[df.columns[i]] = df[df.columns[i]].replace('ND',0).astype('float')
-    
-print(df.dtypes)
+def isStationary():
+    test =  sm.tsa.stattools.adfuller(CURRENCY_DATA)
+    print(f"adf, p-value, c-values: { test[0]}, {test[1]}, {test[4]}")
+    if test[0]>test[4]['5%']:
+        print('non-stationary series')
+    else:
+        print('stationary series')
 
-mreg.hi('mreg')
-nn.hi('nn')
-ea.hi('ea')
+def differenceSeries():
+    difference_row = CURRENCY_DATA.diff(periods=2).dropna()
+    _ = plt.figure(figsize=(15,10))
+    _ = plt.plot(difference_row)
+    test = sm.tsa.stattools.adfuller(difference_row)
+    print(f"adf, p-value, c-values: { test[0]}, {test[1]}, {test[4]}")
+    plt.show()
+    isStationary()
+    _ = plt.figure(figsize=(18,10))
+    ax_1 = plt.subplot(2,1,1)
+    _ = sm.graphics.tsa.plot_acf(difference_row.values.squeeze(), lags=50, ax=ax_1)
+    ax_2 = plt.subplot(2,1,2)
+    _ = sm.graphics.tsa.plot_pacf(difference_row.values.squeeze(), lags=50, ax=ax_2)
+
+defaultPlot()
+autoAndPartcorrelation()
+isStationary()
+differenceSeries()
--- modules/regression.py
+++ modules/regression.py
@@ -1,2 +1,21 @@
-def hi(name):
-    print(name+"suka")
+from modules.imports import *
+from ..main import df
+
+
+def defaultPlot():
+    plt.figure(figsize=(16,8))
+    plt.plot(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'])
+    plt.show()
+
+def autoAndPartcorrelation():
+    _ = plt.figure(figsize=(18,10))
+    ax_1 = plt.subplot(2,1,1)
+    _ = sm.graphics.tsa.plot_acf(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'].values.squeeze(), lags=50, ax=ax_1)
+    ax_2 = plt.subplot(2,1,2)
+    _ = sm.graphics.tsa.plot_pacf(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'].values.squeeze(), lags=50, ax=ax_2)
+    plt.show()
+
+def isStationar():
+
+    print(f"adf, p-value, c-values: { sm.tsa.stattools.adfuller(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'])[0]}, {sm.tsa.stattools.adfuller(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'])[1]}, {sm.tsa.stattools.adfuller(df['AUSTRALIA - AUSTRALIAN DOLLAR/US$'])[4]}")
+    
